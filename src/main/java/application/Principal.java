36ea1021308a7337bfaf5d789868206c03d5f5cf
Gustavo Emídio <gemidio2004@gmail.com>
2024-07-24 14:10:40 -0300

    Criando as classes Principal e Descricao

---
+++ src/main/java/application/Principal.java
@@ -0,0 +1,11 @@
+package application;
+
+import entities.Descricao;
+
+public class Principal {
+
+    public static void main(String[] args) {
+
+        Descricao.descricaoJogo();
+    }
+}
---
+++ src/main/java/entities/Descricao.java
@@ -0,0 +1,14 @@
+package entities;
+
+public class Descricao {
+
+    public static void descricaoJogo(){
+        System.out.println("Bem-vindo(a) ao jogo de adivinhação de números!\n");
+        System.out.println("O jogo consiste em adivinhar um número inteiro entre -30 e 30. ");
+        System.out.println("Ou seja, o número pode estar entre -30 e 0 ou entre 1 e 30. ");
+        System.out.println("Você tem 4 tentativas. A cada tentativa, será exibido o ");
+        System.out.println("número de tentativas restantes e uma dica para lhe auxiliar. ");
+        System.out.println("Se acertar, você ganha o jogo. Caso contrário, perde!\n");
+
+    }
+}
